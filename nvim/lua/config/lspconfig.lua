49463a54b7b21662f124445dd43b355c99d35938
ketzelmania <kalamet_helveta@proton.me>
2024-12-28 14:30:08 -0600

    added nvim

---
+++ nvim/lua/config/lspconfig.lua
@@ -0,0 +1,42 @@
+local lspconfig = require("lspconfig")
+
+-- default config servers
+local servers = {
+	"pyright",
+	"ts_ls",
+	"html",
+	"cssls",
+	"clangd",
+	"lua_ls",
+	"luau_lsp",
+	"texlab",
+}
+-- lsps with default config
+for _, lsp in ipairs(servers) do
+	lspconfig[lsp].setup({
+		capabilities = vim.lsp.protocol.make_client_capabilities(),
+	})
+end
+
+-- rust with clippy
+lspconfig.rust_analyzer.setup({
+	capabilities = vim.lsp.protocol.make_client_capabilities(),
+	settings = {
+		["rust-analyzer"] = {
+			check = {
+				command = "clippy",
+			},
+		},
+	},
+})
+
+-- pyright
+--[[
+do
+	local capabilities = vim.lsp.protocol.make_client_capabilities()
+	capabilities.textDocument.publishDiagnostics.tagSupport.valueSet = { 2 }
+	lspconfig.pyright.setup({
+		capabilities = capabilities,
+	})
+end
+]]
